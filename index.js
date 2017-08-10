9e5547d9d11d1d6eabd91a90e334e0bc95074cf9
Stephen Lester <stephen@lester.codes>
2017-08-09 20:37:32 -0700

    attempt to upsert the old fashioned way if no constraint on the model

--- index.js
+++ index.js
@@ -2,35 +2,48 @@ const _ = require("lodash");
 
 module.exports = bookshelf => {
   bookshelf.Model = bookshelf.Model.extend({
+
     upsert(updateAttributes) {
       const knex = bookshelf.knex;
-      // constraints are the current model's attributes
+      // 'constraint' is the current model's attached constraint
       // updateAttributes are what we want to change if they already exist
       const queryAttributes = {};
+      // this only works with =s
+      // XXX: and probably "IS NULL"/"IS NOT NULL"? oh sheez.
       this._knex._statements.forEach(c => {
-        console.log(c.operator, c.column, c.value);
         if (c.operator === "=") {
           queryAttributes[c.column] = c.value;
+        } else {
+          throw new Error("Invalid operator for upsertion");
         }
       });
-      // surround constraints with double quotes
-      const constraints = Object.keys(queryAttributes).map(c => `"${c}"`);
 
-      // no constraints, ruh roh
-      if (!constraints.length) {
+      const insertionObject = _.extend({}, queryAttributes, updateAttributes);
 
+      // if there aren't any constraints on the model, let's try to find something that matches
+      if (!_.get(this, "constraints", []).length) {
+        return this.where(queryAttributes).fetchAll()
+        .then(results => {
+          if (!results.length) {
+            return this.forge().save(insertionObject, {method: "insert"});
+          } else if (results.length === 1) {
+            return this.forge({id: results.id}).save(updateAttributes, {method: "update", patch: true});
+          } else {
+            throw new Error("Upsert query matches more than one row");
+          }
+        });
+      } else {
+        const constraint = Object.keys(this.constraint).map(c => `"${c}"`);
+        const insert = knex(this.tableName).insert(insertionObject);
+        const update = knex().update(updateAttributes);
+
+        return knex.raw(`? ON CONFLICT (${constraint.join(",")}) DO ? RETURNING *`, [insert, update])
+        .then(x => console.log(x))
+        .catch(e => console.log(e));
       }
+    }
+  },
 
-      console.log(queryAttributes);
-      console.log(updateAttributes);
-      const insert = knex(this.tableName).insert(_.extend({}, queryAttributes, updateAttributes));
-      const update = knex().update(updateAttributes);
-      console.log(constraints.join(","));
-
-      return knex.raw(`? ON CONFLICT (${constraints.join(",")}) DO ? RETURNING *`, [insert, update])
-      .then(x => console.log(x))
-      .catch(e => console.log(e));
-    },
     safeInsert(attributes) {
       const knex = bookshelf.knex;
       const insert = knex(this.tableName).insert(attributes);
