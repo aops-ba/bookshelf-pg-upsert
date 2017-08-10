2d1f48591fb3e28136cde6a4da4ee87f4c49e1fb
Stephen Lester <stephen@artofproblemsolving.com>
2017-08-10 11:18:17 -0700

    remove fallback without constraints

--- index.js
+++ index.js
@@ -9,15 +9,13 @@ module.exports = bookshelf => {
       // updateAttributes are what we want to change if they already exist
       const queryAttributes = {};
 
-      // XXX: what about 'is not null'?
       this._knex._statements.forEach(c => {
         if (c.operator === "=") {
           queryAttributes[c.column] = c.value;
         } else if (c.type === "whereNull") {
           queryAttributes[c.column] = null;
         } else {
-          console.log(c);
-          throw new Error("Invalid operator for upsertion");
+          throw new Error(`Invalid operator on "${c.column}" for upsertion`);
         }
       });
 
@@ -27,24 +25,15 @@ module.exports = bookshelf => {
       constraint = constraint.map(c => `"${c}"`);
 
       // if there aren't any constraints on the model, let's try to find something that matches
-      if (!constraint.length) {
-        return this.where(queryAttributes).fetchAll()
-        .then(results => {
-          if (!results.length) {
-            return knex(this.tableName).insert(insertionObject);
-          } else if (results.length === 1) {
-            return knex(this.tableName).update(updateAttributes).where(queryAttributes);
-          } else {
-            throw new Error("Upsert query matches more than one row");
-          }
-        });
-      } else {
+      if (constraint.length) {
         const insert = knex(this.tableName).insert(insertionObject);
         const update = knex().update(updateAttributes);
 
         return knex.raw(`? ON CONFLICT (${constraint.join(",")}) DO ? RETURNING *`, [insert, update])
         .then(x => console.log(x))
         .catch(e => console.log(e));
+      } else {
+        throw new Error("No constraints on this model");
       }
     },
 
